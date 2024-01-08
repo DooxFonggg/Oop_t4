e778b2a14550f1c185127750f37a02878f1d87b9
DooxFonggg <phongcoi1235@gmail.com>
2024-01-08 15:02:21 +0700

    commit

---
+++ chuong1/chuoikituvaso.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+/**
+ *
+ * @author phong
+ */
+public class chuoikituvaso {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 1.5;
+        String c = "java";
+        
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of c: " + c);
+    }
+}
+
