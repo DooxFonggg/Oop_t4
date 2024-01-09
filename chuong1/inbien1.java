e533c9f88d9394942bdc06f7faaa7e98d3dc4267
DooxFonggg <phongcoi1235@gmail.com>
2024-01-09 11:36:40 +0700

    commit

---
+++ chuong1/inbien1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+/**
+ *
+ * @author phong
+ */
+public class inbien1 {
+    public static void main(String[] args) {
+        int salary = 155;
+        System.out.println(salary);
+    }
+}
