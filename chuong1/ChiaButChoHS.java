2330aade4d3ee5d751395c6151474d7732710b7e
DooxFonggg <phongcoi1235@gmail.com>
2024-01-09 16:10:15 +0700

    commit

---
+++ chuong1/ChiaButChoHS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+/**
+ *
+ * @author phong
+ */
+public class ChiaButChoHS {
+    public static void main(String[] args) {
+
+        // variables to store numbers of pen and student
+        int penNumber = 14;
+        int studentNumber = 3;
+
+        // compute and print the number of pens each student will get
+        // Hint: find the quotient
+        int penPerStudent = 14/3;
+        System.out.println(penPerStudent);
+
+        // compute and print the number of pens that will be left
+        // Hint: find the remainder
+        int remainingPen = 14%3;
+        System.out.println(remainingPen);
+    }
+}
