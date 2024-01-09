5198e9273165a1dbfb9fdd7e006bee2a20ff91e5
DooxFonggg <phongcoi1235@gmail.com>
2024-01-09 16:03:59 +0700

    commit

---
+++ chuong1/toantutru.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+/**
+ *
+ * @author phong
+ */
+public class toantutru {
+    public static void main(String[] args) {
+
+        int costPrice = 129;
+        int sellingPrice = 191;
+
+        // compute the profit and print it
+        int profit =sellingPrice - costPrice;
+        System.out.println(profit);
+    }
+}
