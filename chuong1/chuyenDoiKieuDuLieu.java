ef72a675ebded89448cc0c0147f7fd60e5c0378c
DooxFonggg <phongcoi1235@gmail.com>
2024-01-09 16:18:50 +0700

    commit

---
+++ chuong1/chuyenDoiKieuDuLieu.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+/**
+ *
+ * @author phong
+ */
+public class chuyenDoiKieuDuLieu {
+     public static void main(String[] args) {
+
+        double number = 18.7;
+
+        int result;
+
+        // convert number to integer and add 5
+        result = (int)number + 5 ;
+        System.out.println(result);
+    }
+}
