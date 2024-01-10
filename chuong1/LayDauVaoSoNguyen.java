aa398717a7ef0c1a8732c75e627b93ab6635a221
DooxFonggg <phongcoi1235@gmail.com>
2024-01-10 15:05:31 +0700

    commit

---
+++ chuong1/LayDauVaoSoNguyen.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author phong
+ */
+
+public class LayDauVaoSoNguyen {
+       public static void main(String[] args) {
+     
+        // create object of Scanner
+        Scanner input = new Scanner(System.in);
+    
+        // take input value for number
+        int number = input.nextInt();
+     
+        System.out.println(number);
+     
+        // close the Scanner object
+        input.close();
+    }
+}
+
