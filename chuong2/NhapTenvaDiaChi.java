5c5cc235fe3f5899c9807192f3415ee056155da3
DooxFonggg <phongcoi1235@gmail.com>
2024-01-13 16:11:54 +0700

    commit

---
+++ chuong2/NhapTenvaDiaChi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong2;
+
+/**
+ *
+ * @author phong
+ */
+import java.util.Scanner;
+
+public class NhapTenvaDiaChi {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String address = sc.next();
+		System.out.println("Name: " + name);
+		System.out.println("Address: " + address);
+	}
+}
