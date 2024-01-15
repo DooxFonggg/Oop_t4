0e706ac4eda7d09b43684a32ec1f7061bc720d88
DooxFonggg <phongcoi1235@gmail.com>
2024-01-16 00:01:47 +0700

    commit

---
+++ chuong2/VongLap2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong2;
+
+/**
+ *
+ * @author phong
+ */
+public class VongLap2 {
+    public static void main(String[] args) {
+        // run a for loop from i = 5 to 1
+        // print the value of i
+        // Hint: Use -- operator to decrease the value of i
+        for (int i = 5; i>=1; i--) {
+            System.out.println(i);
+        }
+    }
+}
