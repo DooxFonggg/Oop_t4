db56ab4c0c05433e40708797c2640b5df9ab02ea
DooxFonggg <phongcoi1235@gmail.com>
2024-01-21 16:56:56 +0700

    commit

---
+++ chuong3/TinhDoLechChuan.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author phong
+ */
+public class TinhDoLechChuan {
+        public static void main(String[] args) {
+
+        // array to store numbers
+        double[] numbers = new double[5];
+
+        // get input for numbers
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < numbers.length; ++i) {
+            numbers[i] = input.nextDouble();
+        }
+        
+        double sum = 0.0;
+
+        // find the sum of all the array elements
+        for (int i = 0; i < numbers.length; ++i) {
+            sum += numbers[i];
+        }
+
+        // compute the mean
+        double mean = sum/numbers.length;
+        
+        double result = 0.0;
+        for (int i = 0; i < numbers.length; ++i) {
+
+            // find the difference of each array value and mean
+            double difference = numbers[i] - mean;
+
+            // compute the square of each difference
+            double squaredDifference = Math.pow(difference, 2);
+
+            // add all the square difference 
+            result += squaredDifference;
+        }
+
+        // compute the variance by dividing the above result by numbers.length
+        double variance = result/numbers.length;
+
+        // compute the standard deviation by computing the square root of variance
+        double sd = Math.sqrt(variance);
+        
+        System.out.println(sd);
+
+        input.close();
+    }
+}
