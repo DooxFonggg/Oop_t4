9f91fbd4c8afe231ec2980af55128735433e4c71
DooxFonggg <phongcoi1235@gmail.com>
2024-01-21 16:40:38 +0700

    commit

---
+++ chuong3/inMang.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong3;
+
+/**
+ *
+ * @author phong
+ */
+public class inMang {
+        public static void main(String[] args) {
+
+        // create an array with elements 1, 2, 3, 4, 5
+        int a[] = {1,2,3,4,5};
+
+        // loop through the array to print each elements
+        for (int i = 0; i<a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
