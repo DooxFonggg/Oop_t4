b52209c92168ef30385029f84e1d95c01a2cef60
DooxFonggg <phongcoi1235@gmail.com>
2024-01-23 00:04:46 +0700

    commit

---
+++ chuong4/SdPhuongThucNlan.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong4;
+
+import com.sun.tools.javac.Main;
+
+/**
+ *
+ * @author phong
+ */
+public class SdPhuongThucNlan {
+    
+    // create printNumbers method
+    void printNumbers() {
+        // print 5
+        System.out.println(5);
+        // print 100
+        System.out.println(100);
+  }
+
+    public static void main(String[] args) {
+
+        // creating object of Main class
+        SdPhuongThucNlan obj = new SdPhuongThucNlan(); // tạo đối tượng 
+        // đây còn gọi là phương thức
+    
+        // call printNumbers()
+        obj.printNumbers();
+    
+        // call printNumbers() again
+        obj.printNumbers();
+    }
+}
