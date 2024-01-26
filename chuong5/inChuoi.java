0372fa33521300714452f0fd67a94dd0529a67a6
DooxFonggg <phongcoi1235@gmail.com>
2024-01-26 22:51:59 +0700

    commit

---
+++ chuong5/inChuoi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chuong5;
+
+/**
+ *
+ * @author phong
+ */
+public class inChuoi {
+        public static void main(String[] args) {
+    
+        // create string variable
+        String t = "Slash";
+   
+        // print it
+        System.out.println(t);
+    }
+}
